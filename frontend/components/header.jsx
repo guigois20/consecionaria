1626bd7ff4bd81c4b636026f0fccb8ceff71567a
guilherme <guigois39@gmail.com>
2026-08-29 11:47:25 -0300

    first

---
+++ frontend/components/header.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-black shadow-md shadow-orange-300">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2">
+        <div className="flex max-w-64">
+          <img src="../public/leo motos completo.jpg" alt="leomotos" />
+        </div>
+
+        <div className="flex items-center gap-2 rounded-4xl border-2 px-8 py-2 text-white shadow-md shadow-orange-300">
+          buscar{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </div>
+
+        <div className="flex gap-2 rounded-4xl border-2 px-8 py-2 text-white shadow-md shadow-orange-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+            />
+          </svg>
+          Perfil{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
